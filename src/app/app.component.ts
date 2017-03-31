149d9b426a284fe99983b8a8d5a2eb56ebfee84f
Christopher Grabski <grabc59@gmail.com>
2017-03-31 16:13:17 -0600

    component selector options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css']
   // can also be inline
-  styles: [`
-      h3 {
-        color: dodgerblue;
-      }
-      `]
+  // styles: [`
+  //     h3 {
+  //       color: dodgerblue;
+  //     }
+  //     `]
 })
 export class AppComponent {
   title = 'app works?';
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // you are not limited to selecting tags, you can select by attribute:
+  // selector: '[app-servers]',
+  // or by class:
+  // selector: '.app-servers',
   template: `
     <app-server></app-server>
     <app-server></app-server>`,
