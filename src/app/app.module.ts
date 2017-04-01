324bac712119fe0e414a80eb1dde02a5ca455737
Christopher Grabski <grabc59@gmail.com>
2017-03-31 22:40:44 -0600

    fixed server template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningComponent } from './warning/warning.component';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    WarningComponent
+    WarningComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
