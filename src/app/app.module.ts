c6baa4313fb48a581502cb15f6fd426217303bbe
Christopher Grabski <grabc59@gmail.com>
2017-04-02 15:48:02 -0600

    pull recipes and shopping list up in hierarchy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
-import { ListComponent } from './header/shopping-list/list/list.component';
-import { ShoppingListEditComponent } from './header/shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { RecipeBookComponent } from './header/recipe-book/recipe-book.component';
-import { RecipeListComponent } from './header/recipe-book/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './header/recipe-book/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './header/recipe-book/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ListComponent } from './shopping-list/list/list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
