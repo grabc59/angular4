8ba9cb330dcfb3aeb432d56628a10a0e36714619
Christopher Grabski <grabc59@gmail.com>
2017-04-02 18:17:39 -0600

    setup recipe repeat - exercise

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is a test', 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiKhpqF8YbTAhVk2oMKHe-7DbwQjRwIBw&url=https%3A%2F%2Fpixabay.com%2Fen%2Fphotos%2Frecipe%2F&psig=AFQjCNFY8QxYq2_wZVi8_hzCVravmS8PLw&ust=1491260643246575')
+    new Recipe('A test recipe', 'this is a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Aloo_chat_Recipe.JPG/1280px-Aloo_chat_Recipe.JPG'),
+    new Recipe('Another recipe', 'put the stuff in the thing', 'https://c1.staticflickr.com/6/5235/5906345163_6cf6e65531_b.jpg')
   ];
 
   constructor() { }
