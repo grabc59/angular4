53ad7edc472b96cb90e93a3122f5549c5a652a3d
Christopher Grabski <grabc59@gmail.com>
2017-04-11 17:10:41 -0600

    recipe ingredients are visible in recipe detail

--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from "app/shared/ingredient.model";
 
 @Injectable()
 export class RecipeService {
@@ -7,8 +8,18 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Aloo_chat_Recipe.JPG/1280px-Aloo_chat_Recipe.JPG'),
-    new Recipe('Another recipe', 'put the stuff in the thing', 'https://c1.staticflickr.com/6/5235/5906345163_6cf6e65531_b.jpg')
+    new Recipe('Schnitzel', 
+              'A tasty schnitzel', 'http://kitchenproject.com/german/recipes/Schnitzel/schnitzel.jpg',
+              [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+              ]),
+    new Recipe('Another recipe', 
+              'put the stuff in the thing', 'https://c1.staticflickr.com/6/5235/5906345163_6cf6e65531_b.jpg',
+              [
+                new Ingredient('Bread', 2),
+                new Ingredient('Meat', 1)
+              ])
   ];
 
   getRecipes() {
